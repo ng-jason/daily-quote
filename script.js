db06ad12dc57a2df0876cb6652fa4c9a6c91f36e
Jason Ng <47465498+ng-jason@users.noreply.github.com>
2024-08-22 22:18:40 +1000

    Add formatted date to html

--- script.js
+++ script.js
@@ -26,24 +26,27 @@ document.addEventListener('DOMContentLoaded', function() {
             }
 
             // Update the textContent of an element by ID with today's quote
-            function updateQuote() {
+            function updateContent() {
                 const selectedDate = datePicker.value;
                 const quoteElement = document.getElementById('quote');
                 const authorElement = document.getElementById('author');
+                const dateElement = document.getElementById('date');
                 [quoteElement.textContent, authorElement.textContent] = getQuote(selectedDate);
+                const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+                dateElement.textContent = new Date(selectedDate).toLocaleDateString('en-US', options);
             }
 
             // Update the page with today's quote when the page loads
-            updateQuote();
+            updateContent();
 
             // Event listener to update quote when date changes
-            datePicker.addEventListener('change', updateQuote);
+            datePicker.addEventListener('change', updateContent);
 
             // Event listener to get random quote 
             randomButton.addEventListener('click', () => {
                 const randomDate = dates[Math.floor(Math.random() * dates.length)];
                 datePicker.value = randomDate;
-                updateQuote();
+                updateContent();
             })
         })
         .catch(error => console.error('Error fetching quotes:', error));
